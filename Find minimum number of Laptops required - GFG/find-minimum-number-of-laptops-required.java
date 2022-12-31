3ac3676715f074acf385b35a709ba4ac3927ee3d
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2023-01-01 01:02:23 +0530

    Added solution - LeetHub

---
+++ Find minimum number of Laptops required - GFG/find-minimum-number-of-laptops-required.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            int i = 0;
+            int N = Integer.parseInt(br.readLine().trim());
+
+            int start[] = new int[N];
+            int end[] = new int[N];
+
+            String inputLine2[] = br.readLine().trim().split(" ");
+            String inputLine3[] = br.readLine().trim().split(" ");
+            
+            for (i = 0; i < N; i++) {
+                start[i] = Integer.parseInt(inputLine2[i]);
+                end[i] = Integer.parseInt(inputLine3[i]);
+            }
+            
+            Solution ob = new Solution();
+            System.out.println(ob.minLaptops(N, start, end));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int minLaptops(int n, int[] s, int[] e) {
+        Arrays.sort(s);
+        Arrays.sort(e);
+        int i=0,j=0;
+        int ans=0,count=0;
+        while(i<n){
+            if(s[i]<e[j]){
+                count++;
+                i++;
+            }else{
+                count--;
+                j++;
+            }
+            ans=Math.max(ans,count);
+        }
+        return ans;
+    }
+}
